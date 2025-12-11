e6267309e1aa95f41e51335667e73459202fc724
cbb <33077621+ndlong78@users.noreply.github.com>
2025-12-11 14:03:19 +0700

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
+import ErrorBoundary from './components/ErrorBoundary';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  {/* ➕ WRAP APP BẰNG ErrorBoundary */}
+  <ErrorBoundary>
     <App />
+  </ErrorBoundary>
   </React.StrictMode>
+
 );
